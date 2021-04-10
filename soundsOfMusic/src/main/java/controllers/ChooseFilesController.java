eb6c1fd59cd1008bf70ddf1012b4d7910c1cfd3d
mglushchenko <mfgluschenko@edu.hse.ru>
2021-04-10 19:50:12 +0300

    Added opening new window on proceed button.

--- soundsOfMusic/src/main/java/controllers/ChooseFilesController.java
+++ soundsOfMusic/src/main/java/controllers/ChooseFilesController.java
@@ -3,7 +3,9 @@ package controllers;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
@@ -16,6 +18,7 @@ import javafx.stage.Stage;
 import javafx.util.Callback;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -51,6 +54,27 @@ public class ChooseFilesController {
         }
     }
 
+    private void saveFilesList(MouseEvent mouseEvent) {
+        Node node = (Node) mouseEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        stage.close();
+
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/general_statistics.fxml"));
+            Parent root = loader.load();
+
+            GeneralStatisticsController controller = loader.getController();
+            controller.setFilesList(chosenFiles);
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            System.err.printf("Error: %s%n", e.getMessage());
+        }
+    }
+
     public void proceed(MouseEvent mouseEvent) {
+        saveFilesList(mouseEvent);
     }
 }
