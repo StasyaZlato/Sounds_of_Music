aea228e398adcd43ef44ebac44c2990f8632bd8a
mglushchenko <mfgluschenko@edu.hse.ru>
2021-04-11 12:16:57 +0300

    Resolved conflict.

--- soundsOfMusic/src/main/java/controllers/GeneralStatisticsController.java
+++ soundsOfMusic/src/main/java/controllers/GeneralStatisticsController.java
@@ -1,20 +1,41 @@
 package controllers;
 
+import javafx.beans.property.*;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import models.TableModel;
 
 public class GeneralStatisticsController {
-    @FXML TableView frequencyTable;
+    @FXML TableColumn<TableModel, String> chordColumn;
+    @FXML TableColumn<TableModel, Number> frequencyColumn;
+    @FXML TableView<TableModel> frequencyTable;
+    ObservableList<TableModel> rows = FXCollections.observableArrayList();
 
     public void initialize() {
-        frequencyTable.columnResizePolicyProperty();
-        ObservableList<TableColumn> columns = frequencyTable.getColumns();
-        columns.get(0).prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
-        columns.get(1).prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
+        frequencyTable.setEditable(true);
+        //ObservableList<TableColumn<TableModel, ?>> columns = frequencyTable.getColumns();
+        chordColumn.prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
+        frequencyColumn.prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
 
-        columns.get(0).setResizable(false);
-        columns.get(1).setResizable(false);
+        chordColumn.setResizable(false);
+        frequencyColumn.setResizable(false);
+
+        chordColumn.setCellValueFactory(c -> new SimpleStringProperty("A"));
+        frequencyColumn.setCellValueFactory(c -> new SimpleDoubleProperty(0));
+
+        //columns.get(0).setCellValueFactory(new PropertyValueFactory<TableModel, String>("chord"));
+
+        frequencyTable.setItems(rows);
+    }
+
+    public void onFileSelected(Object selectedFile) {
+        System.out.println(selectedFile);
+        TableModel element = new TableModel(selectedFile.toString(), 0);
+        rows.add(element);
+       // frequencyTable.getItems().add(element);
+       // frequencyTable.setItems(rows);
     }
 }
