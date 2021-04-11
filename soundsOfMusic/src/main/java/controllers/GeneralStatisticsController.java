3b43cab003a2acb8231717adce77793bf5b4e53e
mglushchenko <mfgluschenko@edu.hse.ru>
2021-04-11 14:18:31 +0300

    Changed TableModel to CompositionChord.

--- soundsOfMusic/src/main/java/controllers/GeneralStatisticsController.java
+++ soundsOfMusic/src/main/java/controllers/GeneralStatisticsController.java
@@ -6,36 +6,35 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import models.TableModel;
+import pojo.CompositionChord;
 
 public class GeneralStatisticsController {
-    @FXML TableColumn<TableModel, String> chordColumn;
-    @FXML TableColumn<TableModel, Number> frequencyColumn;
-    @FXML TableView<TableModel> frequencyTable;
-    ObservableList<TableModel> rows = FXCollections.observableArrayList();
+    @FXML TableColumn<CompositionChord, String> chordColumn;
+    @FXML TableColumn<CompositionChord, Number> frequencyColumn;
+    @FXML TableView<CompositionChord> frequencyTable;
+    ObservableList<CompositionChord> rows = FXCollections.observableArrayList();
 
     public void initialize() {
         frequencyTable.setEditable(true);
-        //ObservableList<TableColumn<TableModel, ?>> columns = frequencyTable.getColumns();
         chordColumn.prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
         frequencyColumn.prefWidthProperty().bind(frequencyTable.widthProperty().multiply(0.5));
 
         chordColumn.setResizable(false);
         frequencyColumn.setResizable(false);
 
-        chordColumn.setCellValueFactory(c -> new SimpleStringProperty("A"));
-        frequencyColumn.setCellValueFactory(c -> new SimpleDoubleProperty(0));
+        chordColumn.setCellValueFactory(c -> new SimpleStringProperty(c.getValue().chord));
+        frequencyColumn.setCellValueFactory(c -> new SimpleDoubleProperty(c.getValue().duration));
+        rows.add(new CompositionChord(0, "A"));
 
-        //columns.get(0).setCellValueFactory(new PropertyValueFactory<TableModel, String>("chord"));
 
         frequencyTable.setItems(rows);
     }
 
     public void onFileSelected(Object selectedFile) {
         System.out.println(selectedFile);
-        TableModel element = new TableModel(selectedFile.toString(), 0);
-        rows.add(element);
-       // frequencyTable.getItems().add(element);
-       // frequencyTable.setItems(rows);
+        CompositionChord element = new CompositionChord(0, "A");
+        //rows = FXCollections.observableArrayList();
+        frequencyTable.getItems().add(element);
+        frequencyTable.setItems(rows);
     }
 }
